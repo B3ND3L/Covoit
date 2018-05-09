d386cd44726ecafe581acd962f7565e276d14448
B3ND3L <delahais.b@gmail.com>
2018-05-09 17:07:18 +0200

    Chnage php to full js, add swipe left and right to change the day

--- scripts/main.js
+++ scripts/main.js
@@ -18,11 +18,18 @@ gesuredZone.addEventListener('touchend', function(event) {
 
 function handleGesure() {
     var swiped = 'swiped: ';
-    if (touchendX < touchstartX) {
-        console.log(swiped + 'left!');
-    }
-    if (touchendX > touchstartX) {
+
+    var deltaX = window.innerWidth/10;
+    var deltaY = window.innerHeight/10;
+
+    if (touchendX+deltaX < touchstartX) {
         console.log(swiped + 'right!');
+        swipeDate(getNextDay());
+
+    }
+    if (touchendX-deltaX > touchstartX) {
+        console.log(swiped + 'left!');
+        swipeDate(getPrevDay());
     }
     /*if (touchendY < touchstartY) {
         alert(swiped + 'down!');
@@ -33,4 +40,34 @@ function handleGesure() {
     if (touchendY == touchstartY) {
         console.log('tap!');
     }
+}
+
+function getFullDate(){
+    var weekday=new Array("Dimanche","Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi");
+    var monthname=new Array("Janvier","Fevrier","Mars","Avril","Mai","Juin","Juillet","Août","Septembre","Octobre","Novembre","Décembre");
+
+    return weekday[date.getDay()] + " " +date.getDate() + " " +monthname[date.getMonth()] + " " + date.getFullYear();
+}
+
+function getHeureFin(){
+    return (date.getDay() == 3 || date.getDay() == 5)?'17H15':'17H45';
+}
+
+function getNextDay(){
+    return (date.getDay() == 5)?3:1;
+}
+
+function getPrevDay(){
+    return (date.getDay() == 1)?-3:-1;
+}
+
+function swipeDate(days){
+    date.setDate(date.getDate() + days);
+    refreshDate();
+}
+
+function refreshDate(){
+    $("#date").text(getFullDate(date));
+    $("#retour").text($("#retour").text().replace('???', getHeureFin(date)));
+    $("#retour").text($("#retour").text().replace(/\d{2}H\d{2}/, getHeureFin(date)));
 }
