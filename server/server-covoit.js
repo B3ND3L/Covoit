544fb9c6a6d46cc052ade7b0de170e70ae3a41cb
B3ND3L <delahais.b@gmail.com>
2018-05-10 19:32:02 +0200

    ajout de la dependance mysql et d'une premiere request de test

--- server/server-covoit.js
+++ server/server-covoit.js
@@ -1,4 +1,5 @@
 var http = require('http'),
+	mysql = require('mysql'),
 	WebSocket = require('ws'),
 	WebSocketServer = WebSocket.Server;
 
@@ -7,8 +8,22 @@ server.listen('3000', '0.0.0.0', function() {
   console.log('Listening on ' + server.address().address + ':' + server.address().port);
 });
 
+//BDD covoiturage & bB9XbJkAPAWkNKfD
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "covoiturage",
+  password: "bB9XbJkAPAWkNKfD",
+  database: "covoiturage"
+});
+
 var wss = new WebSocketServer({
 	server: server
+});
+
+con.connect(function(err) {
+	if (err) throw err;
+	console.log("Connected!");
 });
 
 wss.on('connection', function(client) {
@@ -18,7 +33,13 @@ wss.on('connection', function(client) {
     } catch(e){
       console.error('Something wrong append');
       data.type = 'unmanaged';
+			return 0;
     }
     console.log(data);
+	  var sql = "SELECT * FROM Users";
+	  con.query(sql, function (err, result) {
+	    if (err) throw err;
+	    console.log(result);
+	  });
   });
 });
